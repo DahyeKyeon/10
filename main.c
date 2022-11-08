8f9195fcefcb8fa02559e4c08aadbd9e04f89dc7
DahyeKyeon <113089924+DahyeKyeon@users.noreply.github.com>
2022-11-08 17:01:44 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(void){
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+int main(void){
 	
-	strcpy(dst, src);
+	char str[30] = "happy C programming";
 	
-	printf("copied string: %s", dst);
+	printf("the length of string %s: %i\n", str, strlen(str));
 	
 	return 0;
 }
